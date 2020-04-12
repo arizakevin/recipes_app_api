2765224c2fdeb0262a47b5328055c92572731907
kevwas <kevinw.arizas@gmail.com>
2020-04-12 06:21:11 -0500

    enabling cors

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-app.use(cors())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
-
 const bcrypt = require('bcrypt-nodejs')
 const app = express()
+app.use(cors())
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 const { signin } = require('./queries/signin')
 const { register } = require('./queries/register')
@@ -43,12 +30,8 @@ const pool = new Pool({
   connectionString : connString,
   ssl: true
 });
-//fixing syntax error
 
 console.log('connString: ', connString)
-
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json('Node.js, Express, and Postgres API')
