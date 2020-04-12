6fcbd231a775523373e55e64cd451014935cb1dd
kevwas <kevinw.arizas@gmail.com>
2020-04-12 06:13:34 -0500

    enabling cors

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
 const app = express()
 
 const { signin } = require('./queries/signin')
@@ -33,13 +47,6 @@ console.log('connString: ', connString)
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-//app.use(cors())
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get('/', (req, res) => {
   res.json('Node.js, Express, and Postgres API')
