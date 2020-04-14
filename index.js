39faa245bb605c55f2ccb3ca4e57c3b19c5e0871
kevwas <kevinw.arizas@gmail.com>
2020-04-13 21:06:55 -0500

    debug

--- index.js
+++ index.js
@@ -14,8 +14,27 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL, 
-    sslmode: 'require'
-  }
+    ssl: true
+  },
+  pool: {
+    afterCreate: function (conn, done) {
+      // in this example we use pg driver's connection API
+      conn.query('SET timezone="UTC";', function (err) {
+        if (err) {
+          // first query failed, return error and don't try to make next query
+          done(err, conn);
+        } else {
+          // do the second query...
+          conn.query('SELECT set_limit(0.01);', function (err) {
+            // if err is not falsy, connection is discarded from pool
+            // if connection aquire was triggered by a query the error is passed to query promise
+            done(err, conn);
+          });
+        }
+      });
+    }
+  },
+  acquireConnectionTimeout: 10000
 });
 
 process.on('unhandledRejection', (reason, promise) => {
